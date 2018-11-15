49490c2bc923f0c92431b68a093fb67f5dd40468
sascha <sastrand@pdx.edu>
2018-11-15 13:13:03 -0800

    reformatted comments

--- tests.py
+++ tests.py
@@ -1,17 +1,22 @@
 import unittest
 import cap
 
+
 class TestCap(unittest.TestCase):
 
     def setUp(self):
-        # Called before each test method
-        # Allocates exteneral resources used by tests
-        # Like database connections or test data
+        """
+        The setup is called before each test method.
+        It allocates external resources used by tests,
+        like database connections or test data.
+        """
         pass
 
     def tearDown(self):
-        # Run after each test method
-        # Deallocates resources
+        """
+        The tearDown is called after each test method.
+        It's useful to deallocates resources and close connections.
+        """
         pass
 
     def test_one_word(self):
@@ -33,6 +38,10 @@ class TestCap(unittest.TestCase):
         text = "\"you're awesome,\" said daffy duck"
         result = cap.capitalizer(text)
         self.assertEqual(result, "\"You're Awesome,\" Said Daffy Duck")
+
+    # [ADD A TEST HERE]
+
+    # [ADD ANOTHER TEST HERE]
 
 
 if __name__ == '__main__':
