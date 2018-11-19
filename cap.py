1910bd61e92fe6c1d6398b6776157d9964384160
sascha <sastrand@pdx.edu>
2018-11-19 11:37:57 -0800

    removed type conv from example sol

--- cap.py
+++ cap.py
@@ -4,13 +4,10 @@ def first_letter_cap(word):
 	if word[0].isdigit(): 
 		return word
 	elif word[0].isalpha():
-		return list(word[0].capitalize()) + word[1:]
+		return word[0].capitalize() + word[1:]
 	else:
-		return list(word[0]) + first_letter_cap(word[1:])
-
-def first_letter_cap_helper(word):
-	return "".join(first_letter_cap(list(word)))
+		return word[0] + first_letter_cap(word[1:])
 
 def capitalizer(text):
     """Capitalizes the first letter of each word in a string."""
-    return " ".join([first_letter_cap_helper(w) for w in text.split(" ")])
+    return " ".join([first_letter_cap(w) for w in text.split(" ")])
